65f9cdda8dbbac0bbc208d50393da255c6511a88
vladislavs.lapackis <vladislavs.lapackis@accenture.com>
2019-07-03 14:36:41 +0300

    data output fixed

--- src/main/java/com/lms/controllers/MainMenuController.java
+++ src/main/java/com/lms/controllers/MainMenuController.java
@@ -7,20 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lms.models.Author;
 import com.lms.models.Books;
-import com.lms.repository.AuthorRepo;
 import com.lms.repository.BooksRepo;
 
 @Controller
 public class MainMenuController {
 	@Autowired
 	private BooksRepo bookRepo;
-	@Autowired
-	private AuthorRepo aRepo;
 	
 	@RequestMapping("/all")
-	public @ResponseBody List<Author> showAll() {
-		return aRepo.findAll();
+	public @ResponseBody List<Books> showAll() {
+		return bookRepo.findAll();
 	}
 }
